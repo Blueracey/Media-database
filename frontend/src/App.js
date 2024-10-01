582615b001b718c44c346dd565b2dd5c182b8b7d
Ayomide <Ayomide.afolalu@rdpolytech.ca>
2024-10-01 13:25:12 -0600

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AddReview from './components/AddReview';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Media Review Database</h1>
+                <AddReview />
+            </header>
+        </div>
+    );
 }
 
 export default App;
