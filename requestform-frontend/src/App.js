15e0a4c6983c85f40f4d139bd475535dcc6abd2e
Keegan <keeganxdomso@gmail.com>
2024-10-04 15:37:42 -0600

    Initial Button for popup

--- requestform-frontend/src/App.js
+++ requestform-frontend/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import RequestForm from './RequestForm/RequestForm'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the Request Form App</h1>
+        {/* RequestForm  Component */}
+        <RequestForm />
       </header>
     </div>
   );
