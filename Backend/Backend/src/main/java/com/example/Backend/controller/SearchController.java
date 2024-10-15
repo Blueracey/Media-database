5adff79fb569ee6451f533e39787992bfaee2e3f
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-10-15 15:14:47 -0600

    nothing

--- Backend/Backend/src/main/java/com/example/Backend/controller/SearchController.java
+++ Backend/Backend/src/main/java/com/example/Backend/controller/SearchController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping( "/api/v1/search")
 public class SearchController {
--- Backend/Backend/src/main/java/com/example/Backend/entity/Search.java
+++ Backend/Backend/src/main/java/com/example/Backend/entity/Search.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+
 @Entity
 @Data
 @NoArgsConstructor
