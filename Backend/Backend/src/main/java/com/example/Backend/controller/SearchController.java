85a3c60ff96dc01a56a8d383f5edaf533721277c
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-11-22 17:15:51 -0700

    Added Update search
    Added update search it only modefy's the searchID and the name because everything else should not be put in manualy

--- Backend/Backend/src/main/java/com/example/Backend/controller/SearchController.java
+++ Backend/Backend/src/main/java/com/example/Backend/controller/SearchController.java
@@ -25,16 +25,38 @@ public class SearchController {
     public List<Search> getAllSearch() {
         return searchService.getAllSearchs();
     }
+
+    //get by id
     @GetMapping("{id}")
     public ResponseEntity<Search> getSearchById(@PathVariable  int id){
         Search search = searchService.getSearchById(id).orElseThrow(() -> new ResourceNotFoundException("Search was invalid" +id));
         return ResponseEntity.ok(search);
     } //https://www.youtube.com/watch?v=aCZmPgBHc88
 
+    //creates search 
     @PostMapping
     public Search createSearch(@RequestBody Search search){ 
  
         return searchService.createSearch(search);
+    }
+
+
+    //update search only allows for the updating of the name and review ID because everything else need to be done on the server side anyway,
+    // doing it manualy is a holdover of the testing phase
+    @PutMapping("{id}")
+    public ResponseEntity<Search> updatedSearch(@PathVariable int id,@RequestBody Search searchinfo){
+
+        Search newSearch = searchService.getSearchById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("Search was invalid" + id));
+        //newSearch.setId(id);
+        newSearch.setName(searchinfo.getName());
+        newSearch.setReviewId(searchinfo.getReviewId());
+
+        searchService.createSearch(newSearch);
+
+        return ResponseEntity.ok(newSearch);
+
+
     }
 
 
