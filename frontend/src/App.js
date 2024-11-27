f7e18270d876bd9a68c697956ea6448b451625e6
Ayomide <Ayomide.afolalu@rdpolytech.ca>
2024-11-27 02:12:18 -0700

    Add Media-database-Ayomide-Add-Review-Branch

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import AddReview from './components/AddReview';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <AddReview />
+            </header>
+        </div>
+    );
+}
+
+export default App;
+
