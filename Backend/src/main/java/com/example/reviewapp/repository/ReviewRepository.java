d47844afd85329a6c29516e9ec584ee657c9106c
Ayomide <144939674+Ayomide780@users.noreply.github.com>
2024-11-27 00:57:55 -0700

    Delete Backend directory

--- Backend/src/main/java/com/example/reviewapp/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.reviewapp.repository;
-
-import com.example.reviewapp.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
