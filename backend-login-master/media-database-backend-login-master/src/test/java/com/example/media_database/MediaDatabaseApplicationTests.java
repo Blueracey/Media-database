cecba91e4ec01d9ad16c1952b42c10ba897802a2
mfenano789 <fenanomoriba550@gmail.com>
2024-11-27 22:30:03 -0700

    Add backend-login folder

---
+++ backend-login-master/media-database-backend-login-master/src/test/java/com/example/media_database/MediaDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.media_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
